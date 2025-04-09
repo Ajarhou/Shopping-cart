a7ed3efdc1110c0e0ed05a4878709f038d280cce
JARHOU <jarhou06@gmail.com>
2025-04-09 18:23:58 +0200

    mise en place du composant store et ajout de fonctionalité qui de determine le format de la currency

--- my-shopping-cart/src/components/Store.js
+++ my-shopping-cart/src/components/Store.js
@@ -1,9 +1,24 @@
-import React from 'react'
+/* eslint-disable no-unreachable */
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import items from "../data/items.json";
+import StoreItem from "./StoreItem";
 
 const Store = () => {
-  return (
-    <div>Store</div>
-  )
-}
+  return(
+    <>
+    <h1>Store</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {items.map((item) => (
+        <Col key={item.id}>
+          <StoreItem {...item} />
+        </Col>
+      ))}
+    </Row>
+  </>
 
-export default Store
+  )
+ 
+};
+
+export default Store;
